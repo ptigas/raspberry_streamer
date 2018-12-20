65088a9838e248c7396180486048da5c51701e8f
Panagiotis Tigkas <panos.tigas@cartap.co.uk>
2018-12-20 20:28:17 +0000

    simplified the dockers

--- Dockerfiles/entry.sh
+++ Dockerfiles/entry.sh
@@ -2,4 +2,4 @@
 
 echo Streaming url: $1
 
-ffmpeg -f alsa -i default -f v4l2 -i /dev/video0 -s 320x240 -ar 11025 -f flv -r 30.0  rtmp://a.rtmp.youtube.com/live2/$1
+ffmpeg -f alsa -i hw:1,0 -f v4l2 -i /dev/video0 -s 320x240 -ar 11025 -f flv -r 30.0  rtmp://a.rtmp.youtube.com/live2/$1
